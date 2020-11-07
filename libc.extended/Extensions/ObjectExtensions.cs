75074dcec21ac0b7075db1a0c7b6e66e1faeb167
Saeid Farahi <saeidfarahi@gmail.com>
2020-11-07 17:14:25 +0330

    3.1.1 published: ToDictionary Extension method now checks if the object is derived from a dictionary itself

--- libc.extended/Extensions/ObjectExtensions.cs
+++ libc.extended/Extensions/ObjectExtensions.cs
@@ -9,23 +9,30 @@ namespace libc.extended.Extensions {
             if (source == null)
                 throw new NullReferenceException(
                     "Unable to convert anonymous object to a dictionary. The source anonymous object is null.");
+            if (source is IDictionary<string, object>) {
+                return (IDictionary<string, object>) source;
+            }
             var props = source.GetType().GetRuntimeProperties();
             var res = props.ToDictionary(item => item.Name, item => item.GetValue(source));
             return res;
         }
         public static void AssertNotNull(this object item, string msg = "شناسه اشتباه است") {
-            if (item == null) throw new Exception(msg);
+            if (item == null)
+                throw new Exception(msg);
         }
         public static dynamic RemoveProperties(this object source, params string[] properties) {
-            if (properties == null || properties.Length == 0) return source;
-            if (source == null) return null;
+            if (properties == null || properties.Length == 0)
+                return source;
+            if (source == null)
+                return null;
             var dic = source.ToDictionary();
             foreach (var property in properties)
                 if (dic.ContainsKey(property))
                     dic.Remove(property);
             dynamic res = new ExpandoObject();
-            var k = (ICollection<KeyValuePair<string, object>>) res;
-            foreach (var kv in dic) k.Add(kv);
+            var k = (ICollection<KeyValuePair<string, object>>)res;
+            foreach (var kv in dic)
+                k.Add(kv);
             return res;
         }
     }
